efbc0c866471f9537ae4569c36151732c3251b21
Sara Enderborg <sara_enderborg@outlook.com>
2025-11-28 16:27:49 +0100

    this added images and changed styles in projecs

--- src/sections/Projects/Projects.styles.js
+++ src/sections/Projects/Projects.styles.js
@@ -5,9 +5,11 @@ export const Section = styled.section`
 `;
 
 export const Title = styled.h2`
-  font-size: 3rem;
   font-family: ${({ theme }) => theme.fonts.heading};
-  margin-bottom: 3rem;
+  color: ${({ theme }) => theme.colors.text};
+  font-size: 3rem;
+  text-align: center;
+  margin-bottom: 4rem;
 `;
 
 export const Grid = styled.div`
@@ -15,7 +17,11 @@ export const Grid = styled.div`
   grid-template-columns: repeat(3, 1fr);
   gap: 3rem;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.md}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     grid-template-columns: 1fr;
   }
 `;
